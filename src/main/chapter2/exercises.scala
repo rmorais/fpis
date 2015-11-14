1c270686cf23480f85ae0b0ae708c24583197f0f
Rui Morais <rui.morais@shazam.com>
2015-11-14 20:09:59 +0000

    Add solution for exercise 2.2

--- src/main/chapter2/exercises.scala
+++ src/main/chapter2/exercises.scala
@@ -5,11 +5,24 @@ object exercises {
   def fib(n: Int): Int = {
     @tailrec
     def go(n: Int, previous: Int, current: Int): Int = {
-      if (n == 0)
+      if (n <= 1)
         previous
       else
-        go(n-1 , current, previous + current)
+        go(n - 1, current, previous + current)
     }
     go(n, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], p: (A, A) => Boolean): Boolean = {
+    @tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length - 1)
+        true
+      else if (p(as(n), as(n + 1)))
+        loop (n + 1)
+      else
+        false
+    }
+    loop(0)
   }
 }
