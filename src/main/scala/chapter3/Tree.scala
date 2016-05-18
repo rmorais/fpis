0e3033cd6c2d1b0f205afaea95adead46cd1e22a
Rui Morais <rui.morais@gmail.com>
2016-05-18 19:23:43 +0100

    add solution for exercise 3.29

--- src/main/scala/chapter3/Tree.scala
+++ src/main/scala/chapter3/Tree.scala
@@ -38,4 +38,18 @@ object Tree {
     case Leaf(v) => Leaf(f(v))
     case Branch(l,r) => Branch(map(l)(f), map(r)(f))
   }
+
+  //Exercise 3.29
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeViaFold[A](t: Tree[A]): Int = fold(t)(a => 1)(1 + _ + _)
+
+  def maximumViaFold(t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
+
+  def depthViaFold[A](t: Tree[A]): Int = fold(t)(a => 0)((l,r) => 1 + (l max r))
+
+  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))
 }
--- src/test/scala/chapter3/TreeTest.scala
+++ src/test/scala/chapter3/TreeTest.scala
@@ -10,11 +10,13 @@ class TreeTest extends FlatSpec with Matchers {
   "size" should "return 1 given the tree has one leaf" in {
     val tree = Leaf(1)
     Tree.size(tree) should be (1)
+    Tree.sizeViaFold(tree) should be (1)
   }
 
   it should "return 2 given the tree has no branches" in {
     val tree = Branch(Leaf(1), Leaf(2))
     Tree.size(tree) should be (3)
+    Tree.sizeViaFold(tree) should be (3)
   }
 
   it should "return the size of the given tree" in {
@@ -22,6 +24,8 @@ class TreeTest extends FlatSpec with Matchers {
     val tree2 = Branch(Branch(Leaf(0), Leaf(1)), Branch(Leaf(2), Leaf(3)))
     Tree.size(tree) should be (5)
     Tree.size(tree2) should be (7)
+    Tree.sizeViaFold(tree) should be (5)
+    Tree.sizeViaFold(tree2) should be (7)
   }
 
   "maximum" should "return the maximum value of the tree" in {
@@ -29,11 +33,16 @@ class TreeTest extends FlatSpec with Matchers {
     Tree.maximum(Branch(Leaf(1), Leaf(2))) should be(2)
     Tree.maximum(Branch(Branch(Leaf(1), Leaf(3)), Leaf(2))) should be(3)
     Tree.maximum(Branch(Branch(Leaf(1), Leaf(3)), Branch(Leaf(2), Leaf(100)))) should be(100)
+    Tree.maximumViaFold(Leaf(1)) should be(1)
+    Tree.maximumViaFold(Branch(Leaf(1), Leaf(2))) should be(2)
+    Tree.maximumViaFold(Branch(Branch(Leaf(1), Leaf(3)), Leaf(2))) should be(3)
+    Tree.maximumViaFold(Branch(Branch(Leaf(1), Leaf(3)), Branch(Leaf(2), Leaf(100)))) should be(100)
   }
 
   "depth" should "return 0 given the tree only has one leaf" in {
     val tree = Leaf(1)
     Tree.depth(tree) should be(0)
+    Tree.depthViaFold(tree) should be(0)
    }
 
   it should "return the depth of the tree" in {
@@ -41,5 +50,9 @@ class TreeTest extends FlatSpec with Matchers {
     Tree.depth(Branch(Branch(Leaf(1), Leaf(3)), Leaf(2))) should be(2)
     Tree.depth(Branch(Branch(Leaf(1), Leaf(3)), Branch(Leaf(2), Leaf(100)))) should be(2)
     Tree.depth(Branch(Branch(Branch(Leaf(1), Leaf(3)), Leaf(3)), Branch(Leaf(2), Leaf(100)))) should be(3)
+    Tree.depthViaFold(Branch(Leaf(1), Leaf(2))) should be(1)
+    Tree.depthViaFold(Branch(Branch(Leaf(1), Leaf(3)), Leaf(2))) should be(2)
+    Tree.depthViaFold(Branch(Branch(Leaf(1), Leaf(3)), Branch(Leaf(2), Leaf(100)))) should be(2)
+    Tree.depthViaFold(Branch(Branch(Branch(Leaf(1), Leaf(3)), Leaf(3)), Branch(Leaf(2), Leaf(100)))) should be(3)
   }
 }
