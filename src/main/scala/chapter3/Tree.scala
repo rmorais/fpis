817731631f352c912d5c18d1f78504953c91a8ee
Rui Morais <rui.morais@gmail.com>
2016-05-18 19:07:29 +0100

    add solution for exercise 3.28

--- src/main/scala/chapter3/Tree.scala
+++ src/main/scala/chapter3/Tree.scala
@@ -32,4 +32,10 @@ object Tree {
     case Leaf(_) => 0
     case Branch(l,r) => 1 + (depth(l) max depth(r))
   }
+
+  //Exercise 3.28
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+  }
 }
