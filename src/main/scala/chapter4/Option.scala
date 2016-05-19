994e53d1addd258f7a3d160c67f475425201c1f8
Rui Morais <rui.morais@gmail.com>
2016-05-19 12:01:29 +0100

    add solution for exercise 4.5

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -83,5 +83,19 @@ object Option {
   def sequence_1[A](a: List[Option[A]]): Option[List[A]] =
     a.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
 
+  //Exercise 4.5
+  //naive implementation that looks at list twice
+  def traverse[A,B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    sequence (a map f)
+  }
+
+  def traverse[A,B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a match {
+      case Nil => Some(Nil)
+      case (x :: xs) => map2(f(x), traverse(xs)(f))(_ :: _)
+    }
+  }
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(x => x)
 
 }
