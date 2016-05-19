a555d65dad20385d8b10bcb5a4dc2f1cb68e48f7
Rui Morais <rui.morais@gmail.com>
2016-05-19 11:45:42 +0100

    add solution for exercise 4.4

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -68,4 +68,20 @@ object Option {
     a flatMap (a1 => b map(b1 => f(a1,b1)))
   }
 
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    a match {
+      case Nil => None
+      case (maybeX :: xs) => maybeX flatMap(x => sequence(xs) map (x :: _))
+    }
+  }
+
+  /*
+  It can also be implemented using `foldRight` and `map2`. The type annotation on `foldRight` is needed here; otherwise
+  Scala wrongly infers the result type of the fold as `Some[Nil.type]` and reports a type error (try it!). This is an
+  unfortunate consequence of Scala using subtyping to encode algebraic data types.
+  */
+  def sequence_1[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
+
+
 }
