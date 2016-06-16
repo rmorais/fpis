0f4445edf52fdc2219b318fdcdc0e7c9c57e7c32
Rui Morais <rui.morais@gmail.com>
2016-06-16 10:26:45 +0100

    add solution for exercise 4.7

--- src/main/scala/chapter4/Either.scala
+++ src/main/scala/chapter4/Either.scala
@@ -16,31 +16,48 @@ sealed trait Either[+E, +A] {
     }
   }
 
-  def flatMap[EE >:E,B](f: A => Either[EE,B]): Either[EE, B] = {
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
     this match {
       case Right(a) => f(a)
       case Left(e) => Left(e)
     }
   }
 
-  def orElse[EE >:E,B >: A](b: Either[EE,B]): Either[EE, B] = {
+  def orElse[EE >: E, B >: A](b: Either[EE, B]): Either[EE, B] = {
     this match {
       case Left(_) => b
       case Right(a) => Right(a)
     }
   }
 
-  def map2[EE >:E,B,C](b: Either[EE,B])(f: (A,B) => C): Either[EE, C] = {
-    this flatMap(aa => b map(bb => f(aa, bb)))
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    this flatMap (aa => b map (bb => f(aa, bb)))
   }
 
-  def mapViaFor[EE >:E,B,C](b: Either[EE,B])(f: (A,B) => C): Either[EE, C] = {
+  def mapViaFor[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
     for {
       aa <- this
       bb <- b
-    } yield f(aa,bb)
+    } yield f(aa, bb)
   }
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
+
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+
+object Either {
+
+  def traverse[E,A,B](es: List[A])(f: A => Either[E,B]): Either[E, List[B]] = {
+    es match {
+      case Nil => Right(Nil)
+      case x :: xs => f(x).map2(traverse(xs)(f))(_ :: _)
+    }
+  }
+
+  def sequence[E,A](es: List[Either[E,A]]): Either[E, List[A]] = {
+    traverse(es)(identity)
+  }
+}
+
