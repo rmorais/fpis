e7cfcbb7eaa90cde611c6f5390fa5ba4f6b24bbc
Rui Morais <rui.morais@gmail.com>
2016-06-16 11:53:29 +0100

    add solution for exercise 5.2

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -48,6 +48,17 @@ still _pure_.
     go(this)
   }
 
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => Stream.cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => Stream.cons(h(), Empty)
+    case _ => Empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
+
 }
 
 case object Empty extends Stream[Nothing]
---
+++ src/test/scala/chapter5/StreamTest.scala
@@ -0,0 +1,35 @@
+package chapter5
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by rmorais on 16/06/2016.
+  */
+class StreamTest extends FlatSpec with Matchers {
+
+  "take" should "return empty stream if n is zero or less" in {
+    Stream(1 to 10 : _*).take(0) should be(Empty)
+    Stream(1 to 10 : _*).take(-5) should be(Empty)
+  }
+  it should "return a stream with the first n elements" in {
+    Stream(1 to 1000 : _*).take(1).toList should be(Stream(1).toList)
+    Stream(1 to 1000 : _*).take(53).toList should be(Stream(1 to 53: _*).toList)
+  }
+
+  "drop" should "return the stream if n is zero or less" in {
+    Stream(1 to 10 : _*).drop(0).toList should be(Stream(1 to 10: _*).toList)
+    Stream(1 to 10 : _*).drop(-10).toList should be(Stream(1 to 10: _*).toList)
+  }
+
+  it should "return the stream without the first n elements" in {
+    var n = 3
+    Stream(1 to 10 : _*).drop(n).toList should be(Stream(n + 1 to 10: _*).toList)
+    n = 1
+    Stream(1 to 10 : _*).drop(n).toList should be(Stream(n + 1 to 10: _*).toList)
+  }
+
+  it should "return an empty stream if n is bigger than the stream length" in {
+    Stream(1 to 10 : _*).drop(20) should be(Empty)
+  }
+
+}
